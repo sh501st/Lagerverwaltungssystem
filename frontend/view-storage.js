7b276b90fdd960c3d3298bc1388099ff3e34429b
dwdv <dwdv@posteo.de>
2018-05-21 14:42:25 +0200

    Show popup with shelf's inventory on click

--- frontend/view-storage.js
+++ frontend/view-storage.js
@@ -1,10 +1,13 @@
 const Color = Object.freeze({
     DEFAULT: '#f0f0f0',
-    BORDER: '#606060'
+    BORDER: '#606060',
+    HIGHLIGHT: '#30aaee',
+    ACCESS: '#ee3060',
+    OVERLAY: '#909090'
 });
 
-let storage, stage, layer;
-let cols, rows;
+let storage, cols, rows;
+let stage, layer, greyOverlayLayer, popupLayer;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -23,32 +26,70 @@ async function main() {
 	height: height,
 	draggable: true
     });
-    addZoomListener();
+    window.addEventListener('wheel', handleMouseScroll);
 
     layer = new Konva.Layer();
     createStorageBorder();
     createShelves();
     createEntrances();
     stage.add(layer);
+
+    greyOverlayLayer = new Konva.Layer();
+    let block = new Konva.Rect({
+	x: 0,
+	y: 0,
+	width: cols * size,
+	height: rows * size,
+	fill: Color.OVERLAY,
+	opacity: 0.7
+    });
+    greyOverlayLayer.add(block);
+    greyOverlayLayer.hide();
+    stage.add(greyOverlayLayer);
+
+    popupLayer = new Konva.Layer();
+    let popup = new Konva.Rect({
+	id: 'popup',
+	x: cols * size / 4,
+	y: rows * size / 8,
+	width: cols * size / 2,
+	height: rows * size * 0.75,
+	fill: Color.DEFAULT,
+	stroke: Color.BORDER,
+	strokeWidth: 3
+    });
+    const textPadding = 20;
+    let inventoryText = new Konva.Text({
+	id: 'invLabel',
+	x: popup.x() + textPadding,
+	y: popup.y() + textPadding,
+	width: popup.width() - textPadding * 2,
+	height: popup.height() - textPadding * 2,
+	fontSize: 24,
+	fill: Color.BORDER,
+	text: 'EMPTY'
+    });
+    popupLayer.add(popup);
+    popupLayer.add(inventoryText);
+    popupLayer.hide();
+    stage.add(popupLayer);
 }
 
-function addZoomListener() {
-    window.addEventListener('wheel', (e) => {
-	e.preventDefault();
-	const fact = 1.1;
-	const origScale = stage.scaleX();
-	const newScale = e.deltaY < 0 ? origScale * fact : origScale / fact;
-	const mx = stage.getPointerPosition().x;
-	const my = stage.getPointerPosition().y;
-	const nx = (mx - stage.x()) / origScale;
-	const ny = (my - stage.y()) / origScale;
-	stage.scale({ x: newScale, y: newScale });
-	stage.position({
-	    x: mx - nx * newScale,
-	    y: my - ny * newScale
-	});
-	stage.batchDraw();
+function handleMouseScroll(e) {
+    e.preventDefault();
+    const fact = 1.1;
+    const origScale = stage.scaleX();
+    const newScale = e.deltaY < 0 ? origScale * fact : origScale / fact;
+    const mx = stage.getPointerPosition().x;
+    const my = stage.getPointerPosition().y;
+    const nx = (mx - stage.x()) / origScale;
+    const ny = (my - stage.y()) / origScale;
+    stage.scale({ x: newScale, y: newScale });
+    stage.position({
+	x: mx - nx * newScale,
+	y: my - ny * newScale
     });
+    stage.batchDraw();
 }
 
 function createStorageBorder() {
@@ -71,7 +112,52 @@ function createShelves() {
 	    stroke: Color.BORDER,
 	    strokeWidth: 2
 	});
+	rect.on('mouseenter', (e) => {
+	    e.target.fill(Color.HIGHLIGHT);
+	    layer.batchDraw();
+	});
+	rect.on('mouseleave', (e) => {
+	    e.target.fill(Color.DEFAULT);
+	    layer.batchDraw();
+	});
+	rect.on('click', (e) => {
+	    const x = Math.floor(e.target.x() / size);
+	    const y = Math.floor(e.target.y() / size);
+	    let shelf = storage.shelves.find((elem) => {
+		return elem.x === x && elem.y == y;
+	    });
+	    if (shelf) {
+		showShelfInventory(shelf);
+	    }
+	});
 	layer.add(rect);
+    });
+}
+
+function showShelfInventory(shelf) {
+    layer.listening(false);
+    stage.draggable(false);
+    window.removeEventListener('wheel', handleMouseScroll);
+
+    let invLabel = popupLayer.find('#invLabel');
+    let txt = 'Shelf:\n\n';
+    shelf.sub.forEach((obj) => {
+	txt += '  ' + obj.article.name + ': ' + obj.count + '\n';
+    });
+    invLabel.text(txt);
+
+    greyOverlayLayer.show();
+    popupLayer.show();
+    stage.draw();
+
+    stage.on('click', () => {
+	window.addEventListener('wheel', handleMouseScroll);
+	stage.draggable(true);
+	layer.listening(true);
+	popupLayer.hide();
+	greyOverlayLayer.hide();
+	stage.draw();
+	stage.off('click');
     });
 }
 
