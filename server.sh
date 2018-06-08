4be890df877d28379a933e425db59282cd4295cd
dwdv <dwdv@posteo.de>
2018-06-08 10:13:18 +0200

    start mariadb service in dev session (server.sh) if not running

--- server.sh
+++ server.sh
@@ -1,2 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+if $(systemctl is-active --quiet mariadb.service); then
+    echo "MariaDB service is already running."
+else
+    echo "Starting MariaDB service:"
+    systemctl start mariadb.service
+fi
 ./node_modules/.bin/nodemon --ignore data/ server.js
